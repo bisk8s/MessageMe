0dca2ba1aa878e607149ee3d5cf1df26e3801227
xnagpa <xnagpa@outlook.com>
2015-05-24 18:33:25 +0300

    Improve images, create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	Letter.create(from: 'Anonymous', content:'Hey guys, I\'ve been fapping like always, when I first time in my life met a mahoshoujo!', image_url:'http://animediet.net/wp-content/uploads/2008/08/nhk1-cover.jpg' )
+	Letter.create(from: 'Anonymous', content:'OP, you suck! Go get yourself a live!', image_url:'https://qpz6pa.bay.livefilestore.com/y1pMSoSssuqXbHU4-lG9g5PgQCyYuxtLfqB_TB4CRs2WACD-JOYTzt0Sd4FK6m_cfAEwNSOSD9M6zq5pG4JUIG5QA/%5BOyasumi%5D_Welcome_to_the_NHK!_03_The_overimaginative_main_male_protagonist.jpg' )
